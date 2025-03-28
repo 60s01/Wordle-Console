2b7642fe6dfcf3a52192bd5c119ef13a5d682f48
Sazhin Ivan <sazhin.ie@mmtr.ru>
2025-03-28 12:52:07 +0300

    Добавил ограничение на слово из 5 символов

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 class Main{
   final static String InputChar = "Введите слово: ";
   final static String End = "Завершение программмы...";
+  final static String EndWhile = "exit";
+  final static String Limit = "Слово состоит не из 5 букв";
+  
   
   public static void main(String[] args){	
 	Scanner scanner = new Scanner(System.in);
@@ -11,9 +14,13 @@ class Main{
 	do {
 	  p(InputChar);
 	  str = scanner.nextLine();
+	  if(str.length() != 5) {
+		pln(Limit); 
+		break;
+	  }
 	  pln(""); 
-	} while(str.equals("exit") == false);
-	p(End);
+	} while(str.equals(EndWhile) == false);
+	pln(End);
   }
   
   static void p(String str){
